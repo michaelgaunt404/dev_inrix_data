a3672f6393037922b85592d65aedf844ccbf4b47
mike gaunt <mike.gaunt.404@gmail.com>
2024-03-13 18:33:46 -0400

    add github installs

--- code/dev_package_load.R
+++ code/dev_package_load.R
@@ -20,9 +20,18 @@ install.packages("tidyverse")
 library(tidyverse)
 
 c("arrow", "clipr", "datapasta", "datatable", "furrr", "here", "leaflet"
-  ,"mapedit", "mapview", "progressr", "qs", "sf", "tictoc") %>%
+  ,"mapedit", "mapview", "plotly", "progressr", "qs", "sf", "tictoc") %>%
   map(install.packages)
 
 #polars install----
 Sys.setenv(NOT_CRAN = "true")
 install.packages("polars", repos = "https://rpolars.r-universe.dev")
+
+
+devtools::install_github("max-alletsee/rstudio-themes")
+library(rstudiothemes)
+rstudiothemes::install_rstudio_themes(theme = "all_dark")
+
+devtools::install_github(
+  'michaelgaunt404/gauntlet'
+)
